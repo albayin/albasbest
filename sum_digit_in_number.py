cf61b73836a7fbbccc8a4302899749cc0c9b268b
albayin <51753953+albayin@users.noreply.github.com>
2019-07-29 17:50:29 -0700

    Update sum_digit_in_number.py
    add new method with mod and floor built in funtion

--- sum_digit_in_number.py
+++ sum_digit_in_number.py
@@ -9,4 +9,22 @@ def digit_sum_str (x):
   return result
 
 # Use Modulo and Floor division
+def digit_sum (x):
+  if x >= 10:
+    result = x%10
+    temp = x//10
+    while temp > 9:
+      result += temp%10
+      print result
+      temp = temp // 10
+      if temp < 10: 
+        result += temp
+        break
+    else:
+      result = temp + x%10
+  else:
+    result = x
+  return result
 
+#test result
+print digit_sum (89) 
